bcec961e2b121545b2f57890c2f56004de1fe8eb
Konstantin8105 <konstantin8105@gmail.com>
2017-09-15 16:14:20 +0300

    start parse the table

--- ianaParser.go
+++ ianaParser.go
@@ -4,9 +4,12 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
 )
 
 var (
@@ -34,51 +37,92 @@ type RootZone struct {
 
 // GetRootZone - get root zone database
 func GetRootZone() (rz RootZone, err error) {
-	response, err := http.Get(URLRootZoneDb)
+	dat, err := ioutil.ReadFile("page")
 	if err != nil {
-		return rz, err
-	}
 
-	defer func() {
-		err2 := response.Body.Close()
-		if err2 != nil {
-			if err != nil {
-				err = fmt.Errorf("Errors: %v\n%v", err, err2)
-			} else {
-				err = err2
-			}
+		response, err := http.Get(URLRootZoneDb)
+		if err != nil {
+			return rz, err
 		}
-	}()
-	var buffer bytes.Buffer
-	_, err = io.Copy(&buffer, response.Body)
-	if err != nil {
-		return rz, err
+
+		defer func() {
+			err2 := response.Body.Close()
+			if err2 != nil {
+				if err != nil {
+					err = fmt.Errorf("Errors: %v\n%v", err, err2)
+				} else {
+					err = err2
+				}
+			}
+		}()
+		var buffer bytes.Buffer
+		_, err = io.Copy(&buffer, response.Body)
+		if err != nil {
+			return rz, err
+		}
+
+		err = ioutil.WriteFile("page", buffer.Bytes(), 0666)
+		if err != nil {
+			panic(err)
+		}
+		dat = buffer.Bytes()
 	}
 
-	z := html.NewTokenizer(bytes.NewReader(buffer.Bytes()))
+	z := html.NewTokenizer(bytes.NewReader(dat))
 	insideTable := false
+	/*
+			Example of html part
+		    <tr>
+		        <td>
+
+		            <span class="domain tld"><a href="/domains/root/db/aaa.html">.aaa</a></span></td>
+
+		        <td>generic</td>
+		        <td>American Automobile Association, Inc.</td>
+		    </tr>
+	*/
 	for {
 		tt := z.Next()
 		switch tt {
 		case html.ErrorToken:
 			goto END
 		case html.TextToken:
+			/*
+				short := strings.TrimSpace(string(z.Text()))
+				if len(short) < 1 {
+					continue
+				}
+				if short == "\n" {
+					continue
+				}*/
 			if insideTable {
-				fmt.Printf("text --> %s\n", z.Text())
+				fmt.Printf("text --> !%s!\n\n", z.Text())
 			}
 		case html.StartTagToken, html.EndTagToken:
-			a, b := z.TagName()
-			if string(a) != "table" {
-				continue
+			tag, hasAttr := z.TagName()
+			if atom.Lookup(tag) == atom.Table {
+				if insideTable {
+					insideTable = false
+					continue
+				} else {
+					insideTable = true
+					r := z.Raw()
+					if !strings.Contains(string(r), "iana-table") {
+						insideTable = false
+					}
+				}
 			}
 			if insideTable {
-				fmt.Printf("tag --> a %s\n", a)
-				fmt.Printf("tag --> b %v\n", b)
-			}
-			if insideTable {
-				insideTable = false
-			} else {
-				insideTable = true
+				fmt.Printf("tag1 --> tag %s\n", tag)
+				fmt.Printf("tag2 --> hasAttr %v\n", hasAttr)
+				c, d, e := z.TagAttr()
+				fmt.Printf("tag4 --> c %s\n", c)
+				fmt.Printf("tag5 --> d %s\n", d)
+				fmt.Printf("tag6 --> e %v\n", e)
+				g := z.Text()
+				fmt.Printf("tag7 --> g %v\n", g)
+				r := z.Raw()
+				fmt.Printf("tag8 --> raw %s\n", r)
 			}
 		}
 	}
